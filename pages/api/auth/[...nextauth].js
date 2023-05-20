53ad542f5eae66acf44b523eab5c1ab13031a907
patrickjeter <132328678+patrickjeter40@users.noreply.github.com>
2023-05-19 18:45:33 -0700

    Added GitHub as a provider in NextAuth configuration

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,14 +1,19 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google";
+import GitHubProvider from "next-auth/providers/github";
 
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
-        clientId: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
       }),
-    // ...add more providers here
+      GitHubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET
+    }),
   ],
+  
 }
 export default NextAuth(authOptions)
