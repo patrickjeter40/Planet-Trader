f9c6f58c62247dd9e7e1863d14c8f6019304be85
patrickjeter <132328678+patrickjeter40@users.noreply.github.com>
2023-05-28 21:58:59 -0700

    Update MongoDB queries and API endpoint
    Updated the MongoDB queries: We modified the queries to use the correct field names and data types. We replaced the manual references with Database References (DBRefs) to establish relationships between collections.
    
    Modified the API endpoint: The API endpoint was updated to find exoplanets documents based on the "PLANET" property instead of "_id". We used the user's "planetFav" array to query for matching exoplanets.
    
    Added getServerSideProps function: We added a new async function called getServerSideProps that runs before rendering the page. This function retrieves the user's session email and uses it to fetch data from the API endpoint. The fetched data is then passed as props to the dashboard page.
    
    Updated the client-side page: The dashboard page was modified to handle the received exoplanets data and render it accordingly. We also added conditional rendering in case the data fetching fails.

--- pages/api/findUserBookmarks.js
+++ pages/api/findUserBookmarks.js
@@ -1,16 +1,28 @@
-
 import clientPromise from '../../lib/mongodb';
-
 
 export default async (req, res) => {
   try {
+    const { userEmail } = req.query; // Retrieve the userEmail from the query parameter
+
     // Connect to the MongoDB database using clientPromise
     const client = await clientPromise;
     const db = client.db('PlanetTrader');
-    const collection = db.collection('exoplanets');
+    const exoplanetsCollection = db.collection('exoplanets');
+    const usersCollection = db.collection('users');
 
-    
-    const exoplanets = await collection.find().sort({ name: 1 }).limit(6).toArray();
+    // Find the user document matching the email
+    const user = await usersCollection.findOne({ email: userEmail });
+
+    if (!user) {
+      return res.status(200).json([]); // Return an empty array if no user is found
+    }
+
+    // Extract the planetFav array from the user document
+    const planetFav = user.planetFav;
+
+    // Find the exoplanets documents where PLANET property matches a value in planetFav
+    const exoplanets = await exoplanetsCollection.find({ PLANET: { $in: planetFav } }).toArray();
+
     res.status(200).json(exoplanets);
   } catch (error) {
     console.error(error);
--- pages/pt/myPlanets.js
+++ pages/pt/myPlanets.js
@@ -10,47 +10,32 @@ import Typography from '@mui/material/Typography';
 import Link from 'next/link';
 import IconStat from '../../components/common/icon-stat';
 import CardBookmark from '../../components/common/card-bookmark';
-import { useSession } from "next-auth/react"
+import { getSession } from 'next-auth/react';
 
-export default function dashboard({ exoplanets }) {
-  
- 
+export default function Dashboard({ exoplanets }) {
   if (!exoplanets) {
     return <p>Unable to fetch data... Please contact support@exoplanets</p>;
   }
 
   const [selectedExoplanetId, setSelectedExoplanetId] = useState(null);
-  const { data: session } = useSession();
-  
 
   return (
     <div class="main">
       <Header title="Dashboard" />
       <Box sx={{ flexGrow: 1 }} className="grid-mt">
-        
-        <Typography variant='h5'>
-            <b>My Planets</b>
+        <Typography variant="h5">
+          <b>My Planets</b>
         </Typography>
         <Grid container spacing={2}>
           {exoplanets.map((exoplanet) => (
             <Grid item xs={4} key={exoplanet.id}>
               <Card sx={{ maxWidth: 500 }}>
-              <Link href={`/pt/details?id=${exoplanet._id}`} onClick={() => setSelectedExoplanetId(exoplanet._id)}>
-                <CardMedia sx={{ height: 140 }} 
-                    image="https://drive.google.com/uc?id=1UTmqzmlYJYMFoa4F-QAWPFFiAhdqe1dN" 
-                    title="" 
-                />
-              </Link>
+                <Link href={`/pt/details?id=${exoplanet._id}`} onClick={() => setSelectedExoplanetId(exoplanet._id)}>
+                  <CardMedia sx={{ height: 140 }} image="https://drive.google.com/uc?id=1UTmqzmlYJYMFoa4F-QAWPFFiAhdqe1dN" title="" />
+                </Link>
                 <CardContent>
                   <Typography gutterBottom variant="h5" component="div">
-                    <IconStat
-                      exoplanet={exoplanet}
-                      id='planet'
-                      svgurl='http://www.w3.org/2000/svg' 
-                      alt='planet-icon'
-                      statpull='ERADIUS' //document property name (exoplanets.ERADIUS)
-                      statcontent='x' //string content to follow data
-                    />
+                    <IconStat exoplanet={exoplanet} id="planet" svgurl="http://www.w3.org/2000/svg" alt="planet-icon" statpull="ERADIUS" statcontent="x" />
                   </Typography>
                   <Typography variant="body2" color="text.secondary">
                     {exoplanet.Archetype}
@@ -61,16 +46,20 @@ export default function dashboard({ exoplanets }) {
             </Grid>
           ))}
         </Grid>
-        
       </Box>
-      <Footer />      
+      <Footer />
     </div>
   );
 }
-export async function getServerSideProps() {
+
+export async function getServerSideProps(context) {
   try {
-    // Fetch the data from the endpoint using fetch
-    const res = await fetch('http://localhost:3000/api/findUserBookmarks');
+    // Get the user session email
+    const session = await getSession(context);
+    const userEmail = session.user.email;
+
+    // Fetch the data from the endpoint using fetch with user email data
+    const res = await fetch(`http://localhost:3000/api/findUserBookmarks?userEmail=${userEmail}`);
     const exoplanets = await res.json();
 
     // Return the data as props
@@ -84,7 +73,3 @@ export async function getServerSideProps() {
     };
   }
 }
-
-
-
-
