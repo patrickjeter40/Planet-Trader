a73563dd5aa4e558634b38d2639ab41dd0170a3d
patrickjeter <132328678+patrickjeter40@users.noreply.github.com>
2023-06-21 10:48:54 -0700

    Removing legacy pages

--- pages/acct/terms.js
+++ pages/acct/terms.js
@@ -84,21 +84,3 @@ export default function dashboard({ exoplanets }) {
   );
 }
 
-export async function getServerSideProps() {
-  try {
-    // Fetch the data from the endpoint using fetch
-    const res = await fetch('http://localhost:3000/api/getplanets-home');
-    const exoplanets = await res.json();
-
-    // Return the data as props
-    return {
-      props: { exoplanets },
-    };
-  } catch (error) {
-    console.error(error);
-    return {
-      props: { exoplanets: [] },
-    };
-  }
-}
-
