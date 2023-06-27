1571a20feab16e5adc47b99d473db833dc173031
patrickjeter <132328678+patrickjeter40@users.noreply.github.com>
2023-06-27 00:28:30 -0700

    Removing 'Delete Account' btn to resolve a bug

--- pages/acct/account.js
+++ pages/acct/account.js
@@ -43,7 +43,7 @@ export default function account({ exoplanets }) {
                       value={session.user.email}
                   />
                   </Item>
-                  <ModalBtn email={session.user.email} />
+                  
                 </Stack>
               </Box>
             </Box>
