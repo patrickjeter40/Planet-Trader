4f841ee60371c85a8df0301462de847788ebde0c
patrickjeter <132328678+patrickjeter40@users.noreply.github.com>
2024-02-13 19:04:19 -0800

    image rehosting

--- components/common/cardGrid.js
+++ components/common/cardGrid.js
@@ -22,7 +22,7 @@ export default function CardGrid({ exoplanets, gridCaption }) {
               <Card sx={{ maxWidth: 500, minHeight:'300px' }}>
               <Link href={`/pt/details?id=${exoplanet._id}`} onClick={() => setSelectedExoplanetId(exoplanet._id)}>
                 <CardMedia sx={{ height: 140 }} 
-                    image="https://drive.google.com/uc?id=1UTmqzmlYJYMFoa4F-QAWPFFiAhdqe1dN" 
+                    image="https://previews.dropbox.com/p/thumb/ACIjjYFbXVsXVmHTlTknkcxc-6QSk1RoUW1XCsEZqgS4XmYF737f9JTq9hq64G6B1xnceVm_DWch9bl7uO78DEjKrc0MX_fnKrXZQ4lGFstIiXzjWAB4MnUms6tjJg5CIm8DuV5qrN-APtSqnkHkHdUEim0CLRuud96jX6eYGMBL9I_8MNVk96J9ACGcB00jj2VFaWRgyAGgn1m0mltW3LVcRiBeB-Lm2ap3sbZiEYMXgLfw_2fcRJx6I_V4SSE7xVxYsRrZZWriKUGLYwX7EC8oi_BC1XE8fMlZz0ySA9Pm72HOVP3JWyW4R5WuzF7KnCbY9aKG-8yXfKqPheZqCG9L/p.jpeg" 
                     title="" 
                 />
               </Link>
